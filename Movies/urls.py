7319b8442d46a7158754172e3c0b91fa00020179
Ewa Czechowska <ewa@ai-traders.com>
2019-10-16 01:24:25 +0200

    rating stars experiment

--- Movies/urls.py
+++ Movies/urls.py
@@ -7,4 +7,5 @@ app_name = 'movie'
 urlpatterns = [
     url(r'^collect_ratings/$', views.collect_ratings, name='collect_ratings'),
     url(r'^first_movies_category/$', views.first_movies_category, name='first_movies_category'),
+    url(r'^test/$', views.test, name='test'),
 ]
